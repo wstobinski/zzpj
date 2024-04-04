59b9467f0293e74f1d16451b3067b94d7432da1f
Nutritious-prog <artur.wlodarczyk12@gmail.com>
2024-04-04 12:56:22 +0200

    Initial commit of basic application setup.

---
+++ project/handballeague/src/main/java/com/handballleague/HandballeagueApplication.java
@@ -0,0 +1,13 @@
+package com.handballleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandballeagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandballeagueApplication.class, args);
+    }
+
+}
---
+++ project/handballeague/src/test/java/com/handballleague/HandballeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handballleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandballeagueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
