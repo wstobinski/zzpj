a262d56239a0b7e285e951b5f521ec584113d6d1
Wojciech Stobiński <tallandslim9@gmail.com>
2024-04-14 15:34:46 +0200

    H2 path error fix

--- project/handballeague/src/main/java/com/handballleague/HandballeagueApplication.java
+++ project/handballeague/src/main/java/com/handballleague/HandballeagueApplication.java
@@ -1,14 +1,7 @@
 package com.handballleague;
 
-import com.handballleague.model.POSITIONS;
-import com.handballleague.model.Player;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.LinkedList;
-import java.util.List;
 
 @SpringBootApplication
 public class HandballeagueApplication {
