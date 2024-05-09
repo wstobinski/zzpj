c3ddb8abe1966878c129c3c9e95f66b66c101e10
wzyndul <242575@edu.p.lodz.pl>
2024-05-09 16:19:58 +0200

    Added method to add Team data to database

--- project/handballeague/src/main/java/com/handballleague/initialization/TeamsInitializer.java
+++ project/handballeague/src/main/java/com/handballleague/initialization/TeamsInitializer.java
@@ -1,19 +1,48 @@
 package com.handballleague.initialization;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.handballleague.model.Team;
+import com.handballleague.repositories.TeamRepository;
 import jakarta.annotation.PostConstruct;
 import okhttp3.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class TeamsInitializer {
 
     @Value("${api.key}")
     private String apiKey;
 
+    @Autowired
+    private TeamRepository teamRepository;
+
+    public void addTeamsToDatabase(String jsonData) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            JsonNode rootNode = objectMapper.readTree(jsonData);
+            JsonNode teamsNode = rootNode.get("response");
+
+            for (JsonNode teamNode : teamsNode) {
+                Team team = new Team();
+                team.setUuid(teamNode.get("id").asLong());
+                team.setTeamName(teamNode.get("name").asText());
+
+
+                teamRepository.save(team);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     @PostConstruct
     public void fetchAndFillData() {
-
+        apiKey = "5cd3647c52894e848f3ca0cfa92c186b";
         OkHttpClient client = new OkHttpClient();
 
         HttpUrl url = new HttpUrl.Builder()
@@ -30,7 +59,9 @@ public class TeamsInitializer {
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
-            System.out.println(response.body().string());
+            String responseBody = response.body().string();
+            System.out.println(responseBody);
+            addTeamsToDatabase(responseBody);
         } catch (IOException e) {
             e.printStackTrace();
         }
