7759443b6d7e6434569921af7c875d1b5f1fd674
Wojciech Stobiński <tallandslim9@gmail.com>
2024-05-14 22:37:22 +0200

    Useless method removed - clean code

--- homeworks/clean-code/src/main/java/pl/zzpj/cleancode/DoIt.java
+++ homeworks/clean-code/src/main/java/pl/zzpj/cleancode/DoIt.java
@@ -7,53 +7,50 @@ import java.util.Map.Entry;
 
 public class DoIt {
 
-	Map<Integer, Integer> hashMap = new HashMap<>();
-	private int intMaxVal = Integer.MIN_VALUE;
-	private int intMinVal = Integer.MAX_VALUE;
+    Map<Integer, Integer> hashMap = new HashMap<>();
+    private int intMaxVal = Integer.MIN_VALUE;
+    private int intMinVal = Integer.MAX_VALUE;
 
-	public void runTheSimulation(List<Integer> numbersList) {
-		findMinAndMaxValues(numbersList);
-	}
-	
-	private void findMinAndMaxValues(List<Integer> list) {
+
+    private void findMinAndMaxValues(List<Integer> list) {
         for (Integer integer : list) findMinAndMaxValues(integer);
-	}
-	
-	private void findMinAndMaxValues(Integer number) {
-		hashMap.put(number, hashMap.getOrDefault(number, 0) + 1);
+    }
 
-		intMaxVal = Math.max(intMaxVal, number);
-		intMinVal = Math.min(intMinVal, number);
-	}
-	
-	public double getWeightedMean() {
-		int weightedValuesSum = 0;
-		int weightsSum = 0;
-		for (Entry<Integer, Integer> element : hashMap.entrySet()) {
-			weightsSum += element.getValue();
-			weightedValuesSum += element.getKey() * element.getValue();
-		}
-		return (double) weightedValuesSum /weightsSum;
-	}
-	
-	public int getIntMinVal() {
-		return intMinVal;
-	}
-	
-	public int getIntMaxVal() {
-		return intMaxVal;
-	}
+    private void findMinAndMaxValues(Integer number) {
+        hashMap.put(number, hashMap.getOrDefault(number, 0) + 1);
+
+        intMaxVal = Math.max(intMaxVal, number);
+        intMinVal = Math.min(intMinVal, number);
+    }
+
+    public double getWeightedMean() {
+        int weightedValuesSum = 0;
+        int weightsSum = 0;
+        for (Entry<Integer, Integer> element : hashMap.entrySet()) {
+            weightsSum += element.getValue();
+            weightedValuesSum += element.getKey() * element.getValue();
+        }
+        return (double) weightedValuesSum / weightsSum;
+    }
+
+    public int getIntMinVal() {
+        return intMinVal;
+    }
+
+    public int getIntMaxVal() {
+        return intMaxVal;
+    }
 
 
-	public String getFizzBuzz(int number) {
-		if (number % 3 == 0 && number % 5 == 0) {
-			return "FizzBuzz";
-		} else if (number % 3 == 0) {
-			return "Fizz";
-		} else if (number % 5 == 0) {
-			return "Buzz";
-		} else {
-			return "";
-		}
-	}
+    public String getFizzBuzz(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else {
+            return "";
+        }
+    }
 }
