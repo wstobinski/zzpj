fea6fd125295455347d0345c2d9d25b09ab0cb40
wzyndul <242575@edu.p.lodz.pl>
2024-06-04 17:36:49 +0200

    vertexAI connection

--- project/handballeague/src/main/java/com/handballleague/initialization/PlayersInitializer.java
+++ project/handballeague/src/main/java/com/handballleague/initialization/PlayersInitializer.java
@@ -1,39 +1,66 @@
 package com.handballleague.initialization;
-
+import com.google.cloud.vertexai.VertexAI;
+import com.google.cloud.vertexai.api.Blob;
+import com.google.cloud.vertexai.api.Content;
+import com.google.cloud.vertexai.api.GenerateContentResponse;
+import com.google.cloud.vertexai.api.GenerationConfig;
+import com.google.cloud.vertexai.api.HarmCategory;
+import com.google.cloud.vertexai.api.Part;
+import com.google.cloud.vertexai.api.SafetySetting;
+import com.google.cloud.vertexai.generativeai.ContentMaker;
+import com.google.cloud.vertexai.generativeai.GenerativeModel;
+import com.google.cloud.vertexai.generativeai.PartMaker;
+import com.google.cloud.vertexai.generativeai.ResponseStream;
+import com.google.protobuf.ByteString;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class PlayersInitializer {
+    public static void main(String[] args) throws IOException {
+        try (VertexAI vertexAi = new VertexAI("handball-app-425321", "us-central1"); ) {
+            GenerationConfig generationConfig =
+                    GenerationConfig.newBuilder()
+                            .setMaxOutputTokens(8192)
+                            .setTemperature(1F)
+                            .setTopP(0.95F)
+                            .build();
+            List<SafetySetting> safetySettings = Arrays.asList(
+                    SafetySetting.newBuilder()
+                            .setCategory(HarmCategory.HARM_CATEGORY_HATE_SPEECH)
+                            .setThreshold(SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE)
+                            .build(),
+                    SafetySetting.newBuilder()
+                            .setCategory(HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT)
+                            .setThreshold(SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE)
+                            .build(),
+                    SafetySetting.newBuilder()
+                            .setCategory(HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT)
+                            .setThreshold(SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE)
+                            .build(),
+                    SafetySetting.newBuilder()
+                            .setCategory(HarmCategory.HARM_CATEGORY_HARASSMENT)
+                            .setThreshold(SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE)
+                            .build()
+            );
+            GenerativeModel model =
+                    new GenerativeModel.Builder()
+                            .setModelName("gemini-1.5-pro-001")
+                            .setVertexAi(vertexAi)
+                            .setGenerationConfig(generationConfig)
+                            .setSafetySettings(safetySettings)
+                            .build();
 
-    private static final String REQUEST_BODY_GPT_TURBO = """
-            {
-                "model": "gpt-3.5-turbo",
-                "messages": [
-                    {
-                        "role": "user",
-                        "content": "Create New player with name, last name, age, position"
-                    }
-                ]
-            }
-            """;
+            var text1 = "come up with data for 10 handball players. For each of them  give separated by comma first name, last name, phone number (9 digits without country code), pitch number, is capitan (yes or no - only 1 player can be capitan). List this players in separete lines, numerate them , don't write anything else";
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        String apiKey = "skip";
+            var content = ContentMaker.fromMultiModalData(text1);
+            ResponseStream<GenerateContentResponse> responseStream = model.generateContentStream(content);
 
-        var request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.openai.com/v1/chat/completions"))
-                .header("Content-Type", "application/json")
-                .header("Authorization", "Bearer " + apiKey)
-                .POST(HttpRequest.BodyPublishers.ofString(REQUEST_BODY_GPT_TURBO))
-                .build();
-
-        var client = HttpClient.newHttpClient();
-        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.statusCode());
-        System.out.println(response.body());
-
+            // Do something with the response
+            responseStream.stream().forEach(System.out::println);
+        }
     }
 }
