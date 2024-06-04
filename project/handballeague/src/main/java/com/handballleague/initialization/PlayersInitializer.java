669ea065c86a9e0a70910ccce7ce97ffd959ece5
wzyndul <242575@edu.p.lodz.pl>
2024-06-04 23:23:36 +0200

    Adding players to database

--- project/handballeague/src/main/java/com/handballleague/initialization/PlayersInitializer.java
+++ project/handballeague/src/main/java/com/handballleague/initialization/PlayersInitializer.java
@@ -11,6 +11,10 @@ import com.google.cloud.vertexai.api.Part;
 import com.google.cloud.vertexai.api.SafetySetting;
 import com.google.cloud.vertexai.generativeai.*;
 import com.google.protobuf.ByteString;
+import com.handballleague.model.Player;
+import com.handballleague.repositories.PlayerRepository;
+import com.jayway.jsonpath.internal.filter.ValueNodes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -25,8 +29,10 @@ import java.util.function.Function;
 public class PlayersInitializer {
 
     private final VertexAI vertexAi;
-
-    public PlayersInitializer() {
+    private final PlayerRepository playerRepository;
+    @Autowired
+    public PlayersInitializer(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
         this.vertexAi = new VertexAI("handball-app-425321", "us-central1");
     }
 
@@ -45,7 +51,7 @@ public class PlayersInitializer {
                         .build();
 
         var text1 = String.format("come up with data for %d handball players with %s names. For each of them give" +
-                " separated by comma first name, last name, phone number (9 digits without country code), pitch number (any number between 0 and 99)," +
+                " separated by comma first name, last name, email, phone number (9 digits without country code), pitch number (any number between 0 and 99)," +
                 " is capitan (yes or no - only 1 player can be capitan). List this players in separate lines, numerate" +
                 " them, don't write anything else", numberOfPlayers, nationality);
 
@@ -58,19 +64,38 @@ public class PlayersInitializer {
             throw new RuntimeException(e);
         }
 
-        StringBuilder allContent = new StringBuilder();
+        StringBuilder playerData = new StringBuilder();
         responseStream.stream().forEach(response -> {
             response.getCandidatesList().forEach(candidate -> {
                 candidate.getContent().getPartsList().forEach(part -> {
-                    allContent.append(part.getText());
+                    playerData.append(part.getText());
                 });
             });
         });
-        System.out.println(allContent.toString());
+        List<String> players = new ArrayList<>(Arrays.asList(playerData.toString().split("\n")));
+
+        try {
+            addPlayersToDatabase(players);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void main(String[] args) {
-        PlayersInitializer playersInitializer = new PlayersInitializer();
-        playersInitializer.generatePlayersData("Russian", 5);
+    public void addPlayersToDatabase(List<String> players) throws IOException {
+        for (String player : players) {
+            String[] playerData = player.split(",");
+            String firstName = playerData[0];
+            String lastName = playerData[1];
+            String email = playerData[2];
+            String phoneNumber = playerData[3];
+            int pitchNumber = Integer.parseInt(playerData[4]);
+            boolean isCaptain = playerData[5].equals("yes");
+
+            if (playerRepository.findByEmail(email) == null) {
+                Player newPlayer = new Player(firstName, lastName, phoneNumber, pitchNumber, isCaptain, false);
+                playerRepository.save(newPlayer);
+            }
+        }
     }
+
 }
