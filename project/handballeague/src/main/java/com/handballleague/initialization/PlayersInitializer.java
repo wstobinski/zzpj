92bf20ee799727868e5acd6d405451f3c6b0c95d
wzyndul <242575@edu.p.lodz.pl>
2024-06-04 20:15:39 +0200

    vertexAI connection done, filtering response also done

--- project/handballeague/src/main/java/com/handballleague/initialization/PlayersInitializer.java
+++ project/handballeague/src/main/java/com/handballleague/initialization/PlayersInitializer.java
@@ -1,4 +1,5 @@
 package com.handballleague.initialization;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.vertexai.VertexAI;
 import com.google.cloud.vertexai.api.Blob;
 import com.google.cloud.vertexai.api.Content;
@@ -7,10 +8,7 @@ import com.google.cloud.vertexai.api.GenerationConfig;
 import com.google.cloud.vertexai.api.HarmCategory;
 import com.google.cloud.vertexai.api.Part;
 import com.google.cloud.vertexai.api.SafetySetting;
-import com.google.cloud.vertexai.generativeai.ContentMaker;
-import com.google.cloud.vertexai.generativeai.GenerativeModel;
-import com.google.cloud.vertexai.generativeai.PartMaker;
-import com.google.cloud.vertexai.generativeai.ResponseStream;
+import com.google.cloud.vertexai.generativeai.*;
 import com.google.protobuf.ByteString;
 import java.io.File;
 import java.io.FileInputStream;
@@ -18,40 +16,24 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 public class PlayersInitializer {
+
+
     public static void main(String[] args) throws IOException {
         try (VertexAI vertexAi = new VertexAI("handball-app-425321", "us-central1"); ) {
             GenerationConfig generationConfig =
                     GenerationConfig.newBuilder()
-                            .setMaxOutputTokens(8192)
-                            .setTemperature(1F)
-                            .setTopP(0.95F)
+                            .setMaxOutputTokens(2048)
+                            .setTemperature(0.9F)
+                            .setTopP(1F)
                             .build();
-            List<SafetySetting> safetySettings = Arrays.asList(
-                    SafetySetting.newBuilder()
-                            .setCategory(HarmCategory.HARM_CATEGORY_HATE_SPEECH)
-                            .setThreshold(SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE)
-                            .build(),
-                    SafetySetting.newBuilder()
-                            .setCategory(HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT)
-                            .setThreshold(SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE)
-                            .build(),
-                    SafetySetting.newBuilder()
-                            .setCategory(HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT)
-                            .setThreshold(SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE)
-                            .build(),
-                    SafetySetting.newBuilder()
-                            .setCategory(HarmCategory.HARM_CATEGORY_HARASSMENT)
-                            .setThreshold(SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE)
-                            .build()
-            );
             GenerativeModel model =
                     new GenerativeModel.Builder()
-                            .setModelName("gemini-1.5-pro-001")
+                            .setModelName("gemini-1.0-pro-001")
                             .setVertexAi(vertexAi)
                             .setGenerationConfig(generationConfig)
-                            .setSafetySettings(safetySettings)
                             .build();
 
             var text1 = "come up with data for 10 handball players. For each of them  give separated by comma first name, last name, phone number (9 digits without country code), pitch number, is capitan (yes or no - only 1 player can be capitan). List this players in separete lines, numerate them , don't write anything else";
@@ -59,8 +41,17 @@ public class PlayersInitializer {
             var content = ContentMaker.fromMultiModalData(text1);
             ResponseStream<GenerateContentResponse> responseStream = model.generateContentStream(content);
 
-            // Do something with the response
-            responseStream.stream().forEach(System.out::println);
+            StringBuilder allContent = new StringBuilder();
+            responseStream.stream().forEach(response -> {
+                response.getCandidatesList().forEach(candidate -> {
+                    candidate.getContent().getPartsList().forEach(part -> {
+                        allContent.append(part.getText());
+                    });
+                });
+            });
+            System.out.println(allContent.toString());
+
+
         }
     }
 }
