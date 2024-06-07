0eb3fead65cd2b44010464086fa2d079e6dafaea
JStepnicki <j.stepnicki2002@gmail.com>
2024-06-07 13:42:52 +0200

    Update PlayersInitializer.java

--- project/handballeague/src/main/java/com/handballleague/initialization/PlayersInitializer.java
+++ project/handballeague/src/main/java/com/handballleague/initialization/PlayersInitializer.java
@@ -1,84 +1,82 @@
 package com.handballleague.initialization;
 
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.vertexai.VertexAI;
-import com.google.cloud.vertexai.api.Blob;
-import com.google.cloud.vertexai.api.Content;
-import com.google.cloud.vertexai.api.GenerateContentResponse;
-import com.google.cloud.vertexai.api.GenerationConfig;
-import com.google.cloud.vertexai.api.HarmCategory;
-import com.google.cloud.vertexai.api.Part;
-import com.google.cloud.vertexai.api.SafetySetting;
-import com.google.cloud.vertexai.generativeai.*;
-import com.google.protobuf.ByteString;
 import com.handballleague.model.Player;
 import com.handballleague.repositories.PlayerRepository;
-import com.jayway.jsonpath.internal.filter.ValueNodes;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
 
 @Service
 public class PlayersInitializer {
 
     private final VertexAI vertexAi;
     private final PlayerRepository playerRepository;
-    @Autowired
+
+    private String API_KEY;
+
+    //    @Autowired
     public PlayersInitializer(PlayerRepository playerRepository) {
         this.playerRepository = playerRepository;
         this.vertexAi = new VertexAI("handball-app-425321", "us-central1");
+        Dotenv dotenv = Dotenv.load();
+        this.API_KEY = dotenv.get("API_KEY");
     }
 
-    public void generatePlayersData(String nationality, int numberOfPlayers) {
-        GenerationConfig generationConfig =
-                GenerationConfig.newBuilder()
-                        .setMaxOutputTokens(2048)
-                        .setTemperature(0.9F)
-                        .setTopP(1F)
-                        .build();
-        GenerativeModel model =
-                new GenerativeModel.Builder()
-                        .setModelName("gemini-1.0-pro-001")
-                        .setVertexAi(vertexAi)
-                        .setGenerationConfig(generationConfig)
-                        .build();
+    public void generatePlayersData(String nationality, int numberOfPlayers) throws Exception {
+        String url = "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=" + API_KEY;
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-        var text1 = String.format("come up with data for %d handball players with %s names. For each of them give" +
-                " separated by comma first name, last name, email, phone number (9 digits without country code), pitch number (any number between 0 and 99)," +
-                " is capitan (yes or no - only 1 player can be capitan). List this players in separate lines, numerate" +
-                " them, don't write anything else", numberOfPlayers, nationality);
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+
+        String jsonInputString = getString(nationality, numberOfPlayers);
 
 
-        var content = ContentMaker.fromMultiModalData(text1);
-        ResponseStream<GenerateContentResponse> responseStream = null;
-        try {
-            responseStream = model.generateContentStream(content);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        con.setDoOutput(true);
+        OutputStream os = con.getOutputStream();
+        os.write(jsonInputString.getBytes());
+        os.flush();
+        os.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("Response Code : " + responseCode);
+
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
         }
 
-        StringBuilder playerData = new StringBuilder();
-        responseStream.stream().forEach(response -> {
-            response.getCandidatesList().forEach(candidate -> {
-                candidate.getContent().getPartsList().forEach(part -> {
-                    playerData.append(part.getText());
-                });
-            });
-        });
-        List<String> players = new ArrayList<>(Arrays.asList(playerData.toString().split("\n")));
+        in.close();
+        con.disconnect();
+        System.out.println("Response Content : " + content.toString());
+
+        List<String> players = new ArrayList<>(Arrays.asList(content.toString().split("\n")));
 
         try {
             addPlayersToDatabase(players);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String getString(String nationality, int numberOfPlayers) {
+        var text = String.format("come up with data for %d handball players with %s names. For each of them give" +
+                " separated by comma first name,last name,email,phone number (9 digits without country code),pitch number (any number between 0 and 99)," +
+                "is capitan (yes or no - only 1 player can be capitan). List this players in separate lines, numerate" +
+                " them, don't write anything else", numberOfPlayers, nationality);
+
+        return String.format("{ \"contents\":[ { \"parts\":[{\"text\": \"%s\"}]} ]}", text);
     }
 
     public void addPlayersToDatabase(List<String> players) throws IOException {
@@ -96,6 +94,7 @@ public class PlayersInitializer {
                 playerRepository.save(newPlayer);
             }
         }
-    }
 
+
+    }
 }
