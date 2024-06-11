d27eae053754a83b82168e64fcfd69de3e482f73
Kamil <kamilgimp99@gmail.com>
2024-06-11 10:58:28 +0200

    Integration test added

---
+++ project/handballeague/src/test/java/com/handballleague/servicesTests/EmailServiceIntegrationTest.java
@@ -0,0 +1,73 @@
+package com.handballleague.servicesTests;
+
+import com.handballleague.HandballeagueApplication;
+import com.handballleague.exceptions.InvalidArgumentException;
+import  com.handballleague.exceptions.ObjectNotFoundInDataBaseException;
+import  com.handballleague.model.User;
+import  com.handballleague.repositories.UserRepository;
+import  com.handballleague.services.EmailService;
+import  org.junit.jupiter.api.Test;
+import  org.springframework.beans.factory.annotation.Autowired;
+import  org.springframework.boot.test.context.SpringBootTest;
+import  org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import  org.springframework.test.annotation.DirtiesContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class EmailServiceIntegrationTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void activateAcc_WithValidCodeAndPass_ReturnsUser() {
+        // Given
+        int code = 123456;
+        String password = "securePassword";
+        User user = new User("john.doe@example.com", password, "user");
+        user.setCode(code);
+        userRepository.save(user);
+
+        // When
+        User activatedUser = emailService.activateAcc(code, password);
+
+        // Then
+        assertThat(activatedUser).isNotNull();
+        assertThat(activatedUser.isActive()).isTrue();
+        assertThat(activatedUser.getCode()).isEqualTo(0);
+    }
+
+    @Test
+    void activateAcc_WithInvalidPassword_ThrowsException() {
+        // Given
+        int code = 123456;
+        String password = null;
+        User user = new User("john.doe@example.com", "somePassword", "user");
+        user.setCode(code);
+        userRepository.save(user);
+
+        // When & Then
+        assertThatThrownBy(() -> emailService.activateAcc(code, password))
+                .isInstanceOf(InvalidArgumentException.class)
+                .hasMessageContaining("Passed password is invalid.");
+    }
+
+    @Test
+    void activateAcc_WithInvalidCode_ThrowsException() {
+        // Given
+        int invalidCode = 999999;
+        String password = "securePassword";
+
+        // When & Then
+        assertThatThrownBy(() -> emailService.activateAcc(invalidCode, password))
+                .isInstanceOf(ObjectNotFoundInDataBaseException.class)
+                .hasMessageContaining("User with given code was not found in database.");
+    }
+}
